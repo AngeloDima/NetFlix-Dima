d651777dae4fd126e3a42389be72ba73f581f4b3
AngeloDima <angelodimauro91@gmail.com>
2023-05-24 18:35:16 +0200

    Collego la apy e faccio stampare a console i "dati"

--- src/app/Service/data.service.ts
+++ src/app/Service/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  apyKey = "8d51300dec6f04d30c71b1ce5ce1c37f";
+  url = `https://api.themoviedb.org/3/movie/550?api_key=${this.apyKey}`
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.url)
+
+  }
+
 }
+
+
+
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../Service/data.service';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+
+  movie?: any
+
+  constructor(private movieService: DataService) { }
+  ngOnInit(): void {
+    this.loadData()
+  }
+
+  loadData() {
+    this.movieService.getData().subscribe((movie: any) => {
+      this.movie = movie
+      console.log(movie)
+    })
+  }
 
 }
+
+
